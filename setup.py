3262f74251c01c73ea23fd689164d5a35a11d5ad
Ophuscado <postbox@ophuscado.com>
2022-12-30 02:27:45 +0100

    Minor changes

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="player1dev",
-    version="0.1.0",
-    py_modules=["player1dev"],
+    author_email="postbox@ophuscado.com",
+    author="Ophuscado",
+    description="Framework to build with Flet (Flutter) and FastAPI. Flet is a Python library that allows you to build Flutter apps with Python. FastAPI is a Python library that allows you to build web apps with Python.",
     include_package_data=True,
+    license="BSD-2-Clause",
+    links={
+        "Author": "https://ophuscado.com",
+        "Source": "https://github.com/Ophuscado/py-player1dev",
+        "Tracker": "https://github.com/Ophuscado/py-player1dev/issues",
+    },
+    name="player1dev",
+    py_modules=["player1dev"],
+    version="1.0.1",
 )
